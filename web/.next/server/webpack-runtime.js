263f0bd688eb4f51ea1b1de8820ca4f039905673
Rakshit <rakshit@Rakshits-MBP.mshome.net>
2026-04-18 17:26:32 +0530

    feat: add simple API key test file and elevator pitches
    - Add test-simple-api-key.js with single hardcoded API key
    - Perfect for quick demos and testing
    - Include tech and non-tech elevator pitches in README
    - Update web build artifacts

--- web/.next/server/webpack-runtime.js
+++ web/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("80ee8b97cc14c481")
+/******/ 		__webpack_require__.h = () => ("bdcc85159d880a0a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- web/.next/static/webpack/webpack.847d58b71678d24e.hot-update.js
+++ web/.next/static/webpack/webpack.847d58b71678d24e.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1e9cc58d569f6692"; }
+/******/ 	__webpack_require__.h = function() { return "035da010643dd892"; }
 /******/ }();
 /******/ 
 /******/ }
